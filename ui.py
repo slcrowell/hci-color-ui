533fecbd23d88e130d8cd32a6775ffc4b6f4ccd4
Stephen L. Crowell <crowelsl@rose-hulman.edu>
2022-02-01 12:34:16 -0500

    Changed ui to move to next bus automatically

--- ui.py
+++ ui.py
@@ -1,121 +1,270 @@
 from tkinter import *
 import datetime
 import time
-window = Tk()
-i=0;
+import threading
 
-window.title("Potsdam Transport Company Bus Shedule Manager")
-window.geometry('1000x600')
-lbl = Label(window, font=('Helvetica 12 bold'), text="Operators: Press route icon associated with the bus that has arrived at the stop to record route delays, Arrivals will be broadcast \n over headsets or shown at the bottom right ")
-lbl.grid(column=0, row=0)
+class App(threading.Thread):
 
-# Create a function with one paramter, i.e., of
-# the text you want to show when button is clicked
-def which_button(button_press):
-    # Printing the text when a button is clicked
-    print(button_press)
+    def __init__(self, baseButtons, modButtons):
+        threading.Thread.__init__(self)
+        self.start()
+        self.baseButtons = baseButtons
+        self.modButtons = modButtons
+        self.index = 0;
 
-def stoptimer1():
-    end = time.time()
-    print("red button pressed")
-    gap = end - start;
-    a="reaction time is"
-    print(f"{a}  {gap}")
+    def callback(self):
+        self.root.quit()
 
-def stoptimer2():
-    end = time.time()
-    print("brown button pressed")
-    gap = end - start;
-    a="reaction time is"
-    print(f"{a}  {gap}")
+    def buttonCallback(self, info, i, isBase):
+        if(info[0]["color"] == info[i]["color"]):
+            self.index += 1
+            if(self.index >= len(self.baseButtons)):
+                self.root.destroy()
+            else:
+                self.renderButtons(self.baseButtons[self.index] if isBase else self.modButtons[self.index], isBase)
+            
 
-def stoptimer3():
-    end = time.time()
-    print("green button pressed")
-    gap = end - start;
-    a="reaction time is"
-    print(f"{a}  {gap}")
+    def renderButtons(self, toRender, isBase):    
+        busButton = Button(self.root, bg = toRender[0]["color"], height = 3, width = 10)
+        busButton.place(x = toRender[0]["x"], y = toRender[0]["y"])
 
-def stoptimer4():
-    end = time.time()
-    print("yellow button pressed")
-    gap = end - start;
-    a="reaction time is"
-    print(f"{a}  {gap}")
+        for i in range(1, len(toRender)):
+            button = Button(self.root, bg = toRender[i]["color"], height = 3, width = 10, command = lambda i=i: self.buttonCallback(toRender, i, isBase))
+            button.place(x = toRender[i]["x"], y = toRender[i]["y"])
 
-def stoptimer5():
-    end = time.time()
-    print("blue button pressed")
-    gap = end - start;
-    a="reaction time is"
-    print(f"{a}  {gap}")
-
-def stoptimer6():
-    end = time.time()
-    print("purple button pressed")
-    gap = end - start;
-    a="reaction time is"
-    print(f"{a}  {gap}")
+    def run(self):
+        self.root = Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
 
 
-# Creating and displaying of button b1
-b1 = Button(window, bg = 'red',height = 3, width = 10,
-            command = stoptimer1)
-b1.place(x=10, y=55)
+        self.root.title("Potsdam Transport Company Bus Shedule Manager")
+        self.root.geometry('1000x600')
 
-# Creating and displaying of button b2
-b2 = Button(window, bg = 'goldenrod4',height = 3, width = 10,
-            command = stoptimer2)
-b2.place(x=100, y=55)
+        directions = Label(self.root, font=('Helvetica 12 bold'), text="Operators: Press route icon associated with the bus that has arrived at the stop to record route delays, Arrivals will be broadcast \n over headsets or shown at the bottom right ")
+        directions.grid(column=0, row=0)
 
-# Creating and displaying of button b3
-b3 = Button(window, bg = 'green',height = 3, width = 10,
-            command = stoptimer3)
-b3.place(x=190, y=55)
+        lbl = Label(self.root, font=('Helvetica 12 bold'), text="current bus at stop :")
+        lbl.place(x=70, y=170)
 
-# Creating and displaying of button b4
-b4 = Button(window, bg = 'yellow',height = 3, width = 10,
-            command = stoptimer4)
-b4.place(x=280, y=55)
+        baseTrial = Button(self.root, text="Base Trial", height = 3, width = 10, command=lambda: self.renderButtons(self.baseButtons[self.index], True))
+        baseTrial.place(x = 230, y = 300)
 
+        modTrial = Button(self.root, text="Mod Trial", height = 3, width = 10, command=lambda: self.renderButtons(self.modButtons[self.index], False))
+        modTrial.place(x = 320, y = 300)
 
-# Creating and displaying of button b5
-b5 = Button(window, bg = 'blue',height = 3, width = 10,
-            command = stoptimer5)
-b5.place(x=370, y=55)
+        self.root.mainloop()
 
+buttons = [
+    [{
+        "color": 'red',
+        "x": 230,
+        "y": 160   
+    },
+    {
+        "color": 'red',
+        "x": 10,
+        "y": 55   
+    },
+    {
+        "color": 'goldenrod4',
+        "x": 100,
+        "y": 55   
+    },
+    {
+        "color": 'green',
+        "x": 190,
+        "y": 55   
+    },
+    {
+        "color": 'yellow',
+        "x": 280,
+        "y": 55   
+    },
+    {
+        "color": 'blue',
+        "x": 370,
+        "y": 55   
+    },
+    {
+        "color": 'purple',
+        "x": 460,
+        "y": 55   
+    }],
+    [{
+        "color": 'goldenrod4',
+        "x": 230,
+        "y": 160   
+    },
+    {
+        "color": 'red',
+        "x": 10,
+        "y": 55   
+    },
+    {
+        "color": 'goldenrod4',
+        "x": 100,
+        "y": 55   
+    },
+    {
+        "color": 'green',
+        "x": 190,
+        "y": 55   
+    },
+    {
+        "color": 'yellow',
+        "x": 280,
+        "y": 55   
+    },
+    {
+        "color": 'blue',
+        "x": 370,
+        "y": 55   
+    },
+    {
+        "color": 'purple',
+        "x": 460,
+        "y": 55   
+    }],
+    [{
+        "color": 'green',
+        "x": 230,
+        "y": 160   
+    },
+    {
+        "color": 'red',
+        "x": 10,
+        "y": 55   
+    },
+    {
+        "color": 'goldenrod4',
+        "x": 100,
+        "y": 55   
+    },
+    {
+        "color": 'green',
+        "x": 190,
+        "y": 55   
+    },
+    {
+        "color": 'yellow',
+        "x": 280,
+        "y": 55   
+    },
+    {
+        "color": 'blue',
+        "x": 370,
+        "y": 55   
+    },
+    {
+        "color": 'purple',
+        "x": 460,
+        "y": 55   
+    }],
+    [{
+        "color": 'yellow',
+        "x": 230,
+        "y": 160   
+    },
+    {
+        "color": 'red',
+        "x": 10,
+        "y": 55   
+    },
+    {
+        "color": 'goldenrod4',
+        "x": 100,
+        "y": 55   
+    },
+    {
+        "color": 'green',
+        "x": 190,
+        "y": 55   
+    },
+    {
+        "color": 'yellow',
+        "x": 280,
+        "y": 55   
+    },
+    {
+        "color": 'blue',
+        "x": 370,
+        "y": 55   
+    },
+    {
+        "color": 'purple',
+        "x": 460,
+        "y": 55   
+    }],
+    [{
+        "color": 'blue',
+        "x": 230,
+        "y": 160   
+    },
+    {
+        "color": 'red',
+        "x": 10,
+        "y": 55   
+    },
+    {
+        "color": 'goldenrod4',
+        "x": 100,
+        "y": 55   
+    },
+    {
+        "color": 'green',
+        "x": 190,
+        "y": 55   
+    },
+    {
+        "color": 'yellow',
+        "x": 280,
+        "y": 55   
+    },
+    {
+        "color": 'blue',
+        "x": 370,
+        "y": 55   
+    },
+    {
+        "color": 'purple',
+        "x": 460,
+        "y": 55   
+    }],
+    [{
+        "color": 'purple',
+        "x": 230,
+        "y": 160   
+    },
+    {
+        "color": 'red',
+        "x": 10,
+        "y": 55   
+    },
+    {
+        "color": 'goldenrod4',
+        "x": 100,
+        "y": 55   
+    },
+    {
+        "color": 'green',
+        "x": 190,
+        "y": 55   
+    },
+    {
+        "color": 'yellow',
+        "x": 280,
+        "y": 55   
+    },
+    {
+        "color": 'blue',
+        "x": 370,
+        "y": 55   
+    },
+    {
+        "color": 'purple',
+        "x": 460,
+        "y": 55   
+    }],
+]
 
-# Creating and displaying of button b6
-b6 = Button(window, bg = 'purple',height = 3, width = 10,
-            command = stoptimer6)
-b6.place(x=460, y=55)
-
-
-
-lbl = Label(window, font=('Helvetica 12 bold'), text="current bus at stop :")
-lbl.place(x=70, y=170)
-
-#display the current bus color upon click
-print("summery of user feedback")
-def fun1():
-     global i
-     i=0;
-def fun2():
-    global i
-    i=i+1;
-mylist = ["red", "green", "blue","purple","goldenrod4","yellow"]
-def update_color():
-    color = mylist[i]
-    print(color + "  color bus arrived")
-    b10 = Button(window, bg = color ,height = 3, width = 10)
-    b10.place(x=230, y=160)
-    global start
-    start = time.time()
-    fun2();
-    if i==6:
-        fun1();
-button = Button(window,width=20,font=('Helvetica 12 bold'), text="click here for the next bus", command=update_color)
-button.place(x=50, y=300)
-
-
-window.mainloop()
+app = App(buttons, buttons)
